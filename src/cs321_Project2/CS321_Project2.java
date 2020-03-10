1830167f54dcebf73dbc17193362d57278540b40
GrandByte <nicklas.pinkston@yahoo.com>
2020-03-09 19:56:54 -0500

    Testing to make sure this works

--- src/cs321_Project2/CS321_Project2.java
+++ src/cs321_Project2/CS321_Project2.java
@@ -1,5 +1,11 @@
 package cs321_Project2;
 
+/**
+ * The main file for the program 
+ * @author Nicklas Pinkston
+ * @version 1.1
+ * @since 2020-03-09
+ */
 public class CS321_Project2 {
 	public static void main(String[] args) {
 		System.out.println("Lodaing Menu...");
